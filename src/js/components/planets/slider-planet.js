d5428b9831eca12adf6f635ae4706c8e1449c03d
Andres Longueira <andreslongueirarengifo@gmail.com>
2023-04-13 10:55:32 +0000

    add overflow x

--- src/js/components/planets/slider-planet.js
+++ src/js/components/planets/slider-planet.js
@@ -1,43 +1,11 @@
 import React from "react"
-import Slider from "react-slick";
-import { ArrowBackIos, ArrowForwardIos } from "@mui/icons-material"
 import CardPlanetComponent from "./card-planet";
 
 
 function SliderPlanetsComponent(prop){
-        const settings = {
-            infinite: true,
-            speed: 500,
-            slidesToShow: 4,
-            slidesToScroll: 3,
-            responsive: [
-              {
-                breakpoint: 1024,
-                settings: {
-                  slidesToShow: 2,
-                  slidesToScroll: 2,
-                }
-              },
-              {
-                breakpoint: 600,
-                settings: {
-                  slidesToShow: 1,
-                  slidesToScroll: 1
-                }
-              },
-              {
-                breakpoint: 480,
-                settings: {
-                  slidesToShow: 1,
-                  slidesToScroll: 1
-                }
-              }
-            ],
 
-        };
     return(
         <div className="slider">
-        <Slider {...settings}>
         {prop.obj.map((item,i)=>{
                 return(
                     <div key ={i}>
@@ -46,45 +14,10 @@ function SliderPlanetsComponent(prop){
                 )
             })
         }
-        </Slider>
       </div>
     )
-    /**
-     *         {prop.obj.map((item,i)=>{
-                return(
-                    <div>
-                        <CardComponent img={prop.img}/>
-                    </div>
-                )
-            })
-        }
-     */
 };
 
 
-/*
-function SampleNextArrow(props) {
-    const { className, style, onClick } = props;
-    return (
-      <div
-        className={className}onClick={onClick}
-      >
-        <ArrowForwardIos style={{color: 'blue', fontSize: '30px'}}/>
-      </div>
-    );
-  }
-  
-  
-  function SamplePrevArrow(props) {
-    const { className, style, onClick } = props;
-    return (
-      <div
-        className={className} onClick={onClick}
-      >
-        <ArrowBackIos style={{color: 'blue', fontSize: '30px'}}/>
-      </div>
-    );
-  }
-*/
 
 export default SliderPlanetsComponent;
